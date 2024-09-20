f066c38be70fffa845ad61485335397eb95d4902
Tahir Uddin Ahmed <01tahirahmed@gmail.com>
2024-09-21 00:39:02 +0530

    add a new file -> post-data.php

---
+++ post-data.php
@@ -0,0 +1,51 @@
+<?php 
+$posts = [
+
+ [
+    'user_id' => 1,
+    'title' => 'Featured Post Title',
+    'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?',
+    'category_id' => 2,
+    'tags' => 'development, coding, java, python',
+    'status' => 'published',
+    'comment_count' => 15,
+    'slug' => 'featured-post-title',
+
+ ],
+
+ [
+    'user_id' => 2,
+    'title' => 'C Programming for Beginners: Getting Started',
+    'content' => 'This post introduces the C programming language to beginners, covering basic syntax, data types, control structures, and how to write your first program in C.',
+    'category_id' => 2,
+    'tags' => 'programming, basics, C',
+    'status' => 'published',
+    'comment_count' => 98,
+    'slug' => 'c-programming-for-beginners',
+ ],
+
+ [
+    'user_id' => 1,
+    'title' => 'Introduction to Git: Version Control',
+    'content' => 'Learn how to use Git for version control, including basic commands, repositories, commits, and branches.',
+    'category_id' => 1,
+    'tags' => 'Git, Verson Control',
+    'status' => 'published',
+    'comment_count' => 64,
+    'slug' => 'introduction-to-git',
+ ],
+
+ [
+    'user_id' => 1,
+    'title' => 'JavaScript Basics for Beginners',
+    'content' => 'This post covers the fundamental concepts of JavaScript, including variables, data types, functions, and loops.',
+    'category_id' => 3,
+    'tags' => 'Javascript, Web-development',
+    'status' => 'published',
+    'comment_count' => 134,
+    'slug' => 'javascript-basics-for-beginners',
+ ]
+
+
+
+];
